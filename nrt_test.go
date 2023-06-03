381ec798fc83688310d3db61653ea73e241c64fe
Saulius Šaltenis <31516441+LtLi0n@users.noreply.github.com>
2023-06-03 04:33:35 +0300

    Dereference pointer at comparison with literal constant

--- nrt_test.go
+++ nrt_test.go
@@ -11,10 +11,10 @@ func TestContentTest_GeneratePermutations(t *testing.T) {
 	if len(tests) != 2 {
 		t.Error("tests/calliope.json should not be producing more than 2 permutation!")
 	}
-	if tests[1].Parameters.Model != "2.7B" {
+	if *tests[1].Parameters.Model != "2.7B" {
 		t.Error("2.7B model was not produced in the permutation output!")
 	}
-	if tests[1].Parameters.Prefix != "vanilla" {
+	if *tests[1].Parameters.Prefix != "vanilla" {
 		t.Error("2.7B model should only produce a `Prefix` of `vanilla`")
 	}
 	// Test behaviors when `6B-v3` is added to the permutation for `Model`.
